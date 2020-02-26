0c59e9f685138c7d866e867f551cf15d349946ab
陈昱安 <31612534@qq.com>
2020-02-26 22:52:03 +0800

    修正了编译错误 但不会显示画面
    Signed-off-by: 陈昱安 <31612534@qq.com>

--- include/pros_lvgl_sim/pros-lvgl-sim.hpp
+++ include/pros_lvgl_sim/pros-lvgl-sim.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include "pros_lvgl_sim_conf.h"
 #if USE_PROS_LVGL_SIM == 1
+#define SDL_MAIN_HANDLED //这个似乎WIN下一定要加
 #include "lv_drivers/display/monitor.h"
 #include "lv_drivers/indev/keyboard.h"
 #include "lv_drivers/indev/mouse.h"
--- src/main.cpp
+++ src/main.cpp
@@ -110,24 +110,24 @@ int main()
     hal_init();
     std::thread mainTask(taskLVGL, nullptr);
     initialize();
-    // ncrapi::SimKB simKB;
+    ncrapi::SimKB simKB;
 
-    // pros::Controller master(pros::E_CONTROLLER_MASTER);
-    // pros::Motor left_mtr(1);
-    // pros::Motor right_mtr(2);
+    pros::Controller master(pros::E_CONTROLLER_MASTER);
+    pros::Motor left_mtr(1);
+    pros::Motor right_mtr(2);
 
     while (1)
     {
-        //ncrapi::SimKB::loop(&autonomous, &opcontrol, &competition_initialize, &disabled);
+        ncrapi::SimKB::loop(&autonomous, &opcontrol, &competition_initialize, &disabled);
 
-        // pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
-        //                  (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
-        //                  (pros::lcd::read_buttons() & LCD_BTN_RIGHT) >> 0);
-        // int left = master.get_analog(ANALOG_LEFT_Y);
-        // int right = master.get_analog(ANALOG_RIGHT_Y);
+        pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
+                         (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
+                         (pros::lcd::read_buttons() & LCD_BTN_RIGHT) >> 0);
+        int left = master.get_analog(ANALOG_LEFT_Y);
+        int right = master.get_analog(ANALOG_RIGHT_Y);
 
-        // left_mtr = left;
-        // right_mtr = right;
+        left_mtr = left;
+        right_mtr = right;
         pros::delay(20);
     }
     return 0;
