c01707893126a073831c7a5925dd00a8c4798d47
3038922 <31612534@qq.com>
2020-11-01 21:29:36 +0800

    upgrade lvgl to 7.7.0
    Signed-off-by: 3038922 <31612534@qq.com>

--- src/main.cpp
+++ src/main.cpp
@@ -33,10 +33,10 @@ void on_center_button()
  */
 void initialize()
 {
-    pros::lcd::initialize();
-    pros::lcd::set_text(1, "Hello PROS User!");
+    // pros::lcd::initialize();
+    // pros::lcd::set_text(1, "Hello PROS User!");
 
-    pros::lcd::register_btn1_cb(on_center_button);
+    // pros::lcd::register_btn1_cb(on_center_button);
 }
 
 /**
@@ -89,20 +89,22 @@ void autonomous()
  * operator control task will be stopped. Re-enabling the robot will restart the
  * task, not resume it from where it left off.
  */
+#include "./lv_examples/src/lv_demo_widgets/lv_demo_widgets.h"
 void opcontrol()
 {
-    pros::Controller master(pros::E_CONTROLLER_MASTER);
-    pros::Motor left_mtr(1);
-    pros::Motor right_mtr(2);
+    // pros::Controller master(pros::E_CONTROLLER_MASTER);
+    // pros::Motor left_mtr(1);
+    // pros::Motor right_mtr(2);
+    lv_demo_widgets();
     while (true)
     {
-        pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
-                         (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
-                         (pros::lcd::read_buttons() & LCD_BTN_RIGHT) >> 0);
-        int left = master.get_analog(ANALOG_LEFT_Y);
-        int right = master.get_analog(ANALOG_RIGHT_Y);
-        left_mtr = left;
-        right_mtr = right;
+        //     pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
+        //                      (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
+        //                      (pros::lcd::read_buttons() & LCD_BTN_RIGHT) >> 0);
+        //     int left = master.get_analog(ANALOG_LEFT_Y);
+        //     int right = master.get_analog(ANALOG_RIGHT_Y);
+        //     left_mtr = left;
+        //     right_mtr = right;
         pros::delay(20);
     }
 }
